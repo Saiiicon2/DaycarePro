33c78464f2aa95e3b6296aff47ca625126d1d730
Ahmed Phiri <ahmedphiri90s@gmail.com>
2025-08-09 09:58:11 +0200

    commit test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { useAuth } from "@/hooks/useAuth";
-
+//commit test
 import Landing from "@/pages/landing";
 import Dashboard from "@/pages/dashboard";
 import ParentLookup from "@/pages/parent-lookup";
